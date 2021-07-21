644b7ed9228b5a35eeb59b51aa7b9e565d72dbee
Baltac <mihai-cristian.baltac@capgemini.com>
2021-07-21 20:51:37 +0300

    v1.0.0

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+import Header from '../Navbar/Navbar';
+
+class Main extends Component{
+constructor(props)
+{
+  super (props);
+  this.state = {
+    
+  }
+}
+
+  render() {
+      <div>
+ 
+
+      </div>
+  }
+}
+
+
+
+
+export default Main;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+  
+//import { NavLink } from "react-router-dom";
+
+class Header extends Component {
+
+}
+export default Header;
