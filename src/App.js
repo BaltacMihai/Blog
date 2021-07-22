46f387d2cef7f4d589847d59ad2c4a3514151809
Baltac <mihai-cristian.baltac@capgemini.com>
2021-07-22 14:27:39 +0300

    v1.2.0 Article Page Created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Main from './Components/Main/Main';
 import './App.scss';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <div className="App">
       <Main />
     </div>
-    </BrowserRouter>
+    </HashRouter>
     
   );
 }
--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
-
+import './Article.scss'
 export class Article extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <div>
+      <div className="menu">
+        <h1>{"Article/" + this.props.data.name}</h1>
+         <div className="container">
+        
+        
+        <div className="article">
+         <div className="details">
+        
+           <img src={this.props.data.img} alt="" />
+           <h1>{this.props.data.name}</h1>
+         <div className="date_author">
+           <p>{this.props.data.author}</p>
+         </div>
+           <div className="text">
+             {this.props.data.text}
+           </div>
+
+         </div>
+         <div className="text">
+
+         </div>
+        </div>
         {console.log(this.props.data.name)}
       </div>
+      </div>
+     
     );
   }
 }
